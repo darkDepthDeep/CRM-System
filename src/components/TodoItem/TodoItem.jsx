3badb42fb3c28f2d7eb540944b98b6a61052422c
Arkadiy007 <Arkadii_9292@mail.ru>
2025-08-13 18:13:17 +0300

    removed getTasks in handleCancelEdit

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -34,7 +34,6 @@ export default function TodoItem({ item, getTasks, validateTodoTitle }) {
   };
 
   const handleCancelEdit = () => {
-    getTasks();
     setIsEditMode(false);
     setValidateError("");
   };
